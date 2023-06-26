dac6006283abb1bb2c13e2bc7d62f9d161b8b8de
UCMHCteacher <UCMHCteacher@163.com>
2023-06-27 01:29:35 +0800

    feat: created structure for fileIO

--- include/gameFileIO.h
+++ include/gameFileIO.h
@@ -1,8 +1,12 @@
 #pragma once
 
 #include<string>
+#include<fstream>
+#include<iostream>
 
 using std::string;
+using std::fstream;
+using std::ios_base;
 
 
 class GameFile
@@ -13,5 +17,17 @@ public:
 public:
     GameFile(string fileName,int mode);
     ~GameFile();
-;
+
+private:
+    fstream fileOperator;
+
+public:
+    bool getMatrix(int** matrix);
+    bool putMatrix(int** matrix);
+
+public:
+    int GetLastError();
+    enum {MATRIXFORMATERROR=1, };
+private:
+    int lastError;
 };
--- src/gameFileIO.cpp
+++ src/gameFileIO.cpp
@@ -2,5 +2,40 @@
 
 GameFile::GameFile(string fileName,int mode)
 {
+    switch (mode)
+    {
+    case IN:
+        fileOperator.open(fileName,ios_base::in);
+        break;
+
+    case OUT:
+        fileOperator.open(fileName,ios_base::out);
+        break;
+    
+    default:
+        break;
+    };
+
+    lastError=0;
+}
+
+GameFile::~GameFile(){}
+
+int GameFile::GetLastError()
+{
+    return lastError;
+}
+
+
+
+
+bool GameFile::getMatrix(int** matrix)
+{
     ;
 }
+
+bool GameFile::putMatrix(int** matrix)
+{
+    ;
+}
+
